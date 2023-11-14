f24df1907809ea6fbf07a62f51b6c87bb76f42a3
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-11-14 23:34:08 +1000

    feat: add validation

--- src/components/ClientForm.vue
+++ src/components/ClientForm.vue
@@ -2,24 +2,34 @@
   <form class="client-form" @submit.prevent="submitForm">
     <fieldset class="client-form__fieldset">
       <div class="client-form__group">
-        <label for="surname" class="client-form__label required">Фамилия </label>
+        <label for="surname" class="client-form__label required"
+          >Фамилия
+        </label>
         <input
           id="surname"
           type="text"
-          v-model="surname"
-          required
+          v-model.trim="$v.surname.$model"
           class="client-form__input"
         />
+        <div class="error" v-if="$v.surname.$dirty && !$v.surname.required">
+          Обязательное поле
+        </div>
       </div>
       <div class="client-form__group">
         <label for="name" class="client-form__label required">Имя </label>
         <input
           id="name"
           type="text"
-          v-model="name"
-          required
+          v-model.trim="$v.name.$model"
           class="client-form__input"
         />
+        <div class="error" v-if="$v.name.$dirty && !$v.name.required">
+          Обязательное поле
+        </div>
+        <div class="error" v-if="!$v.name.minLength">
+          Имя должно содержать не менее
+          {{ $v.name.$params.minLength.min }} символов
+        </div>
       </div>
       <div class="client-form__group">
         <label for="patronymic" class="client-form__label">Отчество</label>
@@ -31,25 +41,38 @@
         />
       </div>
       <div class="client-form__group">
-        <label for="birthdate" class="client-form__label required">Дата рождения </label>
+        <label for="birthdate" class="client-form__label required"
+          >Дата рождения
+        </label>
         <input
           id="birthdate"
           type="date"
-          v-model="birthdate"
-          required
+          v-model="$v.birthdate.$model"
           class="client-form__input"
         />
+        <div class="error" v-if="$v.birthdate.$dirty && !$v.birthdate.required">
+          Обязательное поле
+        </div>
       </div>
       <div class="client-form__group">
-        <label for="phone" class="client-form__label required">Номер телефона </label>
+        <label for="phone" class="client-form__label required"
+          >Номер телефона
+        </label>
         <input
           id="phone"
           type="tel"
-          v-model="phone"
-          pattern="[7]{1}[0-9]{10}"
-          required
+          v-model="$v.phone.$model"
+          pattern="^7[0-9]{10}$"
           class="client-form__input"
+          placeholder="+7 (123) 456-78-90"
         />
+        <div class="error" v-if="$v.phone.$dirty && !$v.phone.required">
+          Обязательное поле
+        </div>
+        <div class="error" v-if="!$v.phone.minLength">
+          Телефон должен содержать
+          {{ $v.phone.$params.minLength.min }} цифр и начинаться с 7
+        </div>
       </div>
       <div class="client-form__group">
         <label for="gender" class="client-form__label">Пол</label>
@@ -61,11 +84,11 @@
       </div>
       <div class="client-form__group">
         <label for="clientGroup" class="client-form__label required"
-          >Группа клиентов: </label
-        >
+          >Группа клиентов:
+        </label>
         <select
           id="clientGroup"
-          v-model="clientGroup"
+          v-model="$v.clientGroup.$model"
           multiple
           class="client-form__select"
           size="1"
@@ -74,6 +97,12 @@
           <option value="Проблемные">Проблемные</option>
           <option value="ОМС">ОМС</option>
         </select>
+        <div
+          class="error"
+          v-if="$v.clientGroup.$dirty && !$v.clientGroup.required"
+        >
+          Обязательное поле
+        </div>
       </div>
       <div class="client-form__group">
         <label for="doctor" class="client-form__label">Лечащий врач:</label>
@@ -87,21 +116,11 @@
       <div class="client-form__group">
         <label for="sendSms" class="client-form__label">Отправлять СМС:</label>
         <div class="client-form__option">
-          <input
-            id="sendSms"
-            type="checkbox"
-            v-model="sendSms"
-            class="client-form__checkbox"
-          />
+          <input id="sendSms" type="checkbox" class="client-form__checkbox" />
           <span class="client-form__option-text">Да</span>
         </div>
         <div class="client-form__option">
-          <input
-            id="noSms"
-            type="checkbox"
-            v-model="noSms"
-            class="client-form__checkbox"
-          />
+          <input id="noSms" type="checkbox" class="client-form__checkbox" />
           <span class="client-form__option-text">Нет</span>
         </div>
       </div>
@@ -112,6 +131,8 @@
 </template>
 
 <script>
+import { required, minLength } from "vuelidate/lib/validators";
+
 export default {
   name: "ClientForm",
   data: () => ({
@@ -125,9 +146,43 @@ export default {
     doctor: "",
     noSms: false,
   }),
+  validations: {
+    surname: {
+      required,
+    },
+    name: {
+      required,
+      minLength: minLength(2),
+    },
+    birthdate: {
+      required,
+    },
+    phone: {
+      required,
+      minLength: minLength(11),
+    },
+    clientGroup: {
+      required,
+    },
+  },
   methods: {
     submitForm() {
-      console.log("Данные формы:", this.name, this.email, this.message);
+      const requiredFields = [
+        "surname",
+        "name",
+        "birthdate",
+        "phone",
+        "clientGroup",
+      ];
+
+      const isEmpty = requiredFields.some(field => !this[field]);
+      const errors = requiredFields.filter((field) => this.$v[field].$error);
+
+      if (errors.length > 0 || isEmpty) {
+        console.log("Пожалуйста, заполните все обязательные поля");
+        return;
+      }
+      console.log("Данные формы:", this.surname, this.name, this.birthdate);
     },
   },
 };
